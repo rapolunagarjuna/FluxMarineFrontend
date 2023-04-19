32f6f4666bd4f9ec6464c37bafc7354c499de0c1
Nagarjuna Rapolu <112997527+rapolunagarjuna@users.noreply.github.com>
2023-04-19 00:29:33 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./Pages/LoginPage";
+import SignupPage from "./Pages/SignupPage";
+import RootLayout from "./Pages/Root";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import ErrorPage from "./Pages/Error";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#009688",
+    },
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <LoginPage /> },
+      { path: "/signup", element: <SignupPage /> },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
+}
+
+export default App;
